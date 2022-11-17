8afe777e75e3daae7f8d7bea73ef569129089115
Cooltor <romaindubus86@gmail.com>
2022-11-17 09:19:49 +0100

    first fork

--- Model/Manager.php
+++ Model/Manager.php
@@ -8,7 +8,7 @@
 
                 $pdo = new PDO(
                     // "mysql:host=localhost; port=8080;dbname=boutiquephp", "root", "root",
-                    "mysql:host=localhost;dbname=tp_poo", "root", "root",
+                    "mysql:host=localhost;dbname=tp_poo", "root", "",
                     array(
                         PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
                         PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8' ));
